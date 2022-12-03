24ba257bb0fe7f8cfdfa4af222a7edee5b12e9c7
Natxo Cabre <natxo.cabre@gmail.com>
2022-12-03 19:10:58 +0100

    replaced devenv with flake because it was not possible to install latest version of elixir and it did not install by default make/gcc to compile elixir libraries.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+  
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+  
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+      elixir = pkgs.beam.packages.erlang.elixir;
+    in
+    with pkgs;
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          inotify-tools
+          elixir_1_14
+          postgresql_14
+          glibcLocales
+        ]; 
+      };
+    });
+}
