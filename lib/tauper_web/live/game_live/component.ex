1da227cf8221d676e9e745c0a76a352d0793e511
Natxo Cabre <natxo.cabre@gmail.com>
2024-12-12 00:56:29 +0100

    updated translations

--- lib/tauper_web/live/game_live/component.ex
+++ lib/tauper_web/live/game_live/component.ex
@@ -9,7 +9,7 @@ defmodule TauperWeb.GameLive.Component do
     ~H"""
     <div id="table">
     <%=  for atomic_number <- Enum.concat(1..57, 72..88) do %>
-        <div class={"element element-#{atomic_number} #{if @question.atomic_number == atomic_number do 'selected' end}"}></div>
+        <div class={"element element-#{atomic_number} #{if @question != nil and  @question.atomic_number == atomic_number do 'selected' end}"}></div>
     <% end %>
     </div>
     """
